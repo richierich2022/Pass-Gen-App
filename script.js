037378ddb7200b8f144d70a76b4d621fc0803a48
Pablo Picasso <rishidigital555@gmail.com>
2023-01-16 22:09:11 +0000

    added if statement to check at least one character type is select

--- script.js
+++ script.js
@@ -98,26 +98,38 @@ function isNumber(char) {
     return false;
   }
 
-  return isNaN(char);
+  return !isNaN(char);
 }
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   var password_len = window.prompt("Please choose password length between 10 to 64 charcters");
-  if(isNumber(password_len))
+  //check if it is a number
+  if(isNumber(password_len) != true)
   {
     return alert("please enter a numeric number");
   }
+  //checks if the password len is less than 10 or more than 64
   else if(password_len < 10 || password_len > 64)
   {
     return alert("error password length");
   }
+
+  //Select one or more character type using confirm prompt which has okay or cancel
   var Character_type_lowercase = window.confirm("Would you want any Lowercases?");
   var Character_type_Uppercase = window.confirm("Would you want any Uppercases?");
   var Character_type_Numeric = window.confirm("Would you want any Numerics?");
   var Character_type_Special_characters = window.confirm("Would you want any Special characters($@%&*,etc)");
-
-
+  
+  // added a check to see if at least one of the character type is selected
+  var logic_gate = Character_type_lowercase || Character_type_Uppercase || Character_type_Special_characters || Character_type_Numeric;
+  var choice_arr = [password_len, Character_type_lowercase,Character_type_Uppercase,Character_type_Special_characters,Character_type_Numeric];
+  if(logic_gate === false)
+  {
+    alert("Please select at least one character type");
+  }
+  //returns two values one of the password parameter and the or choice_arr
+  return choice_arr;
 }
 
 // Function for getting a random element from an array
@@ -127,7 +139,8 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-    var Selection = getPasswordOptions();
+    var choice_arr = getPasswordOptions();
+    
 }
 
 // Get references to the #generate element
